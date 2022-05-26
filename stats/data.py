3335e66c756ea570fbbb3af45a19a2434fa7a38a
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-26 10:36:57 +0100

    5

--- stats/data.py
+++ stats/data.py
@@ -4,3 +4,5 @@ import pandas as pd
 
 game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
+for game_file in game_files:
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
