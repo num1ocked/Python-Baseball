84a92aee67b32e4a5624c5b40aaacd498b2d55d1
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-27 10:18:24 +0100

    4

--- stats/attendance.py
+++ stats/attendance.py
@@ -1,3 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
+
+attendance = games.loc[(games['type'] == info') & (games['type'] == 'attendance'), ['year', 'multi3']]
