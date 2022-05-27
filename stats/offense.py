4dddd43944dd8bb4521c610c4445da53aff44365
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-27 15:25:20 +0100

    1

--- stats/offense.py
+++ stats/offense.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
-plays.columns['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
