bb9d18385897148202a899865755bbad7c962992
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-27 10:57:04 +0100

    4

--- stats/pitching.py
+++ stats/pitching.py
@@ -6,3 +6,4 @@ from data import games
 plays = games[games['type'] == 'play']
 strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+strike_outs = reset_index(name='strike_outs')
