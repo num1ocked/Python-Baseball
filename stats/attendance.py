347674d568eb7348d02ad956fea52e09d6d7533e
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-27 10:29:28 +0100

    7

--- stats/attendance.py
+++ stats/attendance.py
@@ -9,4 +9,6 @@ attendance.columns = ['year', 'attendance']
 attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+plt.xlabel(Year)
+plt.ylabel(Attendance)
 plt.show()
