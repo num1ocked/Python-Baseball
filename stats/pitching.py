eb29e986c9d8259f5dff52f6a804d36cefe9c19b
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-27 10:42:19 +0100

    2

--- stats/pitching.py
+++ stats/pitching.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
-plays[plays['event'].str.contains('K')]
+strike_outs = plays[plays['event'].str.contains('K')]
